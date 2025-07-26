5c4421d97bfaedc2bef8de3cdc998c90fdfabf82
mdmusaalkafi <97037598+mdmusaalkafi@users.noreply.github.com>
2025-07-26 20:46:56 +0600

    random writing

--- variable.js
+++ variable.js
@@ -1,2 +1,19 @@
 var name = "kafi";
-console.log(name)
+console.log(name);
+
+console.log(typeof (name));
+
+var number = 999;
+console.log(number);
+
+console.log(typeof (number));
+
+var isTure = false;
+console.log(isTure);
+
+console.log(typeof isTrue);
+
+
+
+
+
